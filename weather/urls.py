b6032b27aded92594ee168d1be4b18cd86cce738
BLR-55 <m-c@tut.by>
2017-08-03 16:55:48 +0300

    ajax added

--- weather/urls.py
+++ weather/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from weather.views import index, weath2, weath_info
+from weather.views import index, weath_info#, ajax_upd
 
 urlpatterns = [
     url(r'^$', index, name='index_view'),
-    url(r'^test/$', weath2),
     url(r'^weather_info/$', weath_info),
+    #url(r'^upd/$', ajax_upd)
 ]
--- weather/views.py
+++ weather/views.py
@@ -6,23 +6,24 @@ from django.http import HttpResponse
 from pyowm import OWM
 
 
+
 # Create your views here.
 
-def weath2(request):
+# def weath2(request):
 
-    API_key = '63ac529deef3fb307c5c5f5d47d4a9de'
-    owm = OWM(API_key)
+#     API_key = '63ac529deef3fb307c5c5f5d47d4a9de'
+#     owm = OWM(API_key)
 
-    obs = owm.weather_at_place('Moscow')
-    w = obs.get_weather()
-    temperature = w.get_temperature('celsius')['temp']
-    wind_speed = w.get_wind()['speed']
-    humidity = w.get_humidity()
-    pressure = w.get_pressure()['press']
+#     obs = owm.weather_at_place('Moscow')
+#     w = obs.get_weather()
+#     temperature = w.get_temperature('celsius')['temp']
+#     wind_speed = w.get_wind()['speed']
+#     humidity = w.get_humidity()
+#     pressure = w.get_pressure()['press']
 
-    t = Template("<html><body>В Москве сейчас {{temperature}} по цельсию, давление : {{pressure}} мм рт ст.</body></html>")
-    html = t.render(Context({'temperature': temperature, 'pressure': pressure}))
-    return HttpResponse(html)
+#     t = Template("<html><body>В Москве сейчас {{temperature}} по цельсию, давление : {{pressure}} мм рт ст.</body></html>")
+#     html = t.render(Context({'temperature': temperature, 'pressure': pressure}))
+#     return HttpResponse(html)
 
 
 def weath_info(request):
@@ -34,31 +35,133 @@ def weath_info(request):
     form = {}
     if request.method == 'POST':
         form['city'] = request.POST.get('city')
+        try: 
+            ct = form['city']
+            obs = owm.weather_at_place(ct)
+            w = obs.get_weather()
+            form['temperature'] = w.get_temperature('celsius')['temp']
+            form['wind_speed'] = w.get_wind()['speed']
+            form['humidity'] = w.get_humidity()
+            form['press'] = w.get_pressure()['press'] 
+
+        except:
+            return render(request, 'other/weather_info.html', {'message': 'Такого города нет'})
+
+
+        return render(request, 'other/weather_info.html', {'city':ct, 'temp':form['temperature'],\
+                                                'wind_s':form['wind_speed'], 'humidity':form['humidity'],\
+                                                'press':form['press']}\
+                                                )
+    else:
+        return redirect('index_view')
+        # html = "<html><body>Блок елсе</body></html>"
+        # return HttpResponse(html)
+
+def index(request):
+    return render(request, 'other/index.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def info(request):
+
+#     API_key = '63ac529deef3fb307c5c5f5d47d4a9de'
+#     owm = OWM(API_key)
+#     errors = []
+#     form = {}
+
+#     if request.method == 'POST':
+#         if request.POST.get('city'):
+#             form['city'] = request.POST.get('city')
+#             ct = form['city']
+#             obs = owm.weather_at_place(ct)
+#             w = obs.get_weather()
+#             form['temperature'] = w.get_temperature('celsius')['temp']
+#             form['wind_speed'] = w.get_wind()['speed']
+#             form['humidity'] = w.get_humidity()
+#             form['press'] = w.get_pressure()['press'] 
+#             return render(request, 'other/weather_info.html', {'city':ct, 'temp':form['temperature'],\
+#                                                 'wind_s':form['wind_speed'], 'humidity':form['humidity'],\
+#                                                 'press':form['press']}\
+#                                                 )
+#         else:
+#             return render(request, 'other/weather_info.html')
+
+
+
+
+
+
+
+# def weath_info(request):
+
+#     API_key = '63ac529deef3fb307c5c5f5d47d4a9de'
+#     owm = OWM(API_key)
+
+#     errors = []
+#     form = {}
+#     if request.method == 'POST':
+#         form['city'] = request.POST.get('city')
         
 
-        if not form['city']:
-            errors.append('Введите город')
-        ct = form['city']
-        obs = owm.weather_at_place(ct)
-        w = obs.get_weather()
-        form['temperature'] = w.get_temperature('celsius')['temp']
-        form['wind_speed'] = w.get_wind()['speed']
-        form['humidity'] = w.get_humidity()
-        form['press'] = w.get_pressure()['press'] 
+#         if not form['city']:
+#             errors.append('Введите город')
+
+#             t = Template("<html><body>В Москве сейчас {{temperature}} по цельсию, давление : {{pressure}} мм рт ст.</body></html>")
+#             html = t.render(Context({'temperature': 10, 'pressure': 20}))
+#             return HttpResponse(html)
+
+#         else:
+#             try: 
+#                 ct = form['city']
+#                 obs = owm.weather_at_place(ct)
+#                 w = obs.get_weather()
+#                 form['temperature'] = w.get_temperature('celsius')['temp']
+#                 form['wind_speed'] = w.get_wind()['speed']
+#                 form['humidity'] = w.get_humidity()
+#                 form['press'] = w.get_pressure()['press'] 
+
+#             except:
+#                 html = "<html><body>Такого города нет</body></html>"
+#                 return HttpResponse(html)
+
 
 
 
       
 
-    return render(request, 'other/weather_info.html', {'errors':errors, 'city':ct, 'temp':form['temperature'],\
-                                                'wind_s':form['wind_speed'], 'humidity':form['humidity'],\
-                                                'press':form['press']}\
-                                                )
+#             return render(request, 'other/weather_info.html', {'errors':errors, 'city':ct, 'temp':form['temperature'],\
+#                                                 'wind_s':form['wind_speed'], 'humidity':form['humidity'],\
+#                                                 'press':form['press']}\
+#                                                 )
+#     else:
+#         t = Template("<html><body>В Москве сейчас {{temperature}} по цельсию, давление : {{pressure}} мм рт ст.</body></html>")
+#         html = t.render(Context({'temperature': 10, 'pressure': 20}))
+#         return HttpResponse(html)
+        # return render(request, 'other/weather_info.html')
 
 
 
-def index(request):
-    return render(request, 'other/index.html')
+
 
 
 
